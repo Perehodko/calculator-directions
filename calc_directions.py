e2aab61dcd5d01491f60b43609aceb41593ad297
Perehodko <n.perehodko@yandex.com>
2020-09-09 17:43:08 +0300

    add beautiful output

--- calc_directions.py
+++ calc_directions.py
@@ -15,7 +15,7 @@ def read(f_obj):
         dict_BL[i] = {"magn_dec": line["magn_dec"], "magn_azimut": line["magn_azimut"],
                       "true_azimuth": line["true_azimuth"], "directions_angle": line["directions_angle"],
                       "gamma_result": line["gamma_result"]}
-    print(dict_BL)
+    # print(dict_BL)
 
 
 def my_print():
@@ -29,17 +29,17 @@ def calculate_parameters():
         correction_of_direction = float(i[1]["magn_dec"]) - float(i[1]["gamma_result"])
         direction_angle = float(i[1]["magn_azimut"]) + correction_of_direction
         print("Магнитный азимут равен: {0}°".format(i[1]["magn_azimut"]))
-        print("Истинный азимут:", true_azimuth)
-        print("Дирекционный угол:", round(direction_angle, 3))
-        print("===============================")
+        print("Истинный азимут: {0}°".format(true_azimuth))
+        print("Дирекционный угол равен равен: {0}°".format(round(direction_angle, 3)))
+        print("\n")
     elif i[1]["true_azimuth"] != "-":
         magn_azim = float(i[1]["true_azimuth"]) - float(i[1]["magn_dec"])
         correction_of_direction = float(i[1]["magn_dec"]) - float(i[1]["gamma_result"])
         dir_angle = magn_azim + correction_of_direction
-        print("Истинный азимут равен: {0}".format(i[1]["true_azimuth"]))
-        print("Магнитный азимут равен:{0}".format(magn_azim))
-        print("Дирекционный угол равен равен:{0}".format(dir_angle))
-        print("+++++++++++++++++++++++++++++++")
+        print("Истинный азимут равен: {0}°".format(i[1]["true_azimuth"]))
+        print("Магнитный азимут равен: {0}".format(magn_azim))
+        print("Дирекционный угол равен равен: {0}".format(round(dir_angle, 3)))
+        print("\n")
     elif i[1]["directions_angle"] != "-":
         correction_of_direction = float(i[1]["magn_dec"]) - float(i[1]["gamma_result"])
         magn_azimuth = float(i[1]["directions_angle"]) - correction_of_direction
@@ -47,7 +47,7 @@ def calculate_parameters():
         print("Дирекционный угол равен: {0}°".format(i[1]["directions_angle"]))
         print("Магнитный азимут равен: {0}".format(round(magn_azimuth, 3)))
         print("Истинный азимут равен: {0}".format(round(true_azimuth, 3)))
-        print("################################")
+        print("\n")
 
 
 if __name__ == "__main__":
